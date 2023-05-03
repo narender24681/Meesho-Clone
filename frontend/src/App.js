443d46dd5357f85207c0606e9bcaf4b0a8b62b57
Narender <narender24681@gmail.com>
2023-05-03 19:08:04 +0530

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { AllRoutes } from './Components/AllRoutes';
+import { NavBar } from './Components/NavBar';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
