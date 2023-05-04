bfdc4ba22d6087a29e6a2a7fc73182425e452efd
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-05-04 21:54:38 +0530

    ss

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import style from "../Style/Footer.module.css"
+const Footer = () => {
+  return (
+    <div className={style.main}>
+    <div className={style.footer}>
+      <img src="https://i.imgur.com/8xjcPoZ.png" alt="" />
+      <img src="https://i.imgur.com/UxjH3Ly.png" alt="" />
+      <img src="https://i.imgur.com/WuUQ5zS.png" alt="" />
+      <img src="https://i.imgur.com/bW10Eje.png" alt="" />
+      <img src="https://i.imgur.com/YKVDhtm.png" alt="" />
+      <img src="https://i.imgur.com/Acb2vzm.png" alt="" />
+      <img src="https://i.imgur.com/7nvNIKu.png" alt="" />
+      <img src="https://i.imgur.com/fgkV9S8.png" alt="" />
+      <img src="https://i.imgur.com/qrNN7oy.png" alt="" />
+      <img src="https://i.imgur.com/LAP9odR.png" alt="" />
+      <img src="https://i.imgur.com/GykCZvU.png" alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default Footer
