ace988ee737e288e606880eb4ac83989719ee0f9
suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2023-05-06 15:59:21 +0530

    Landing Page

--- frontend/src/Components/AllRoutes.js
+++ frontend/src/Components/AllRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../Pages/Home";
+import Generic_Image_Card from "../Pages/Generic_Image_Card";
 
 export const AllRoutes = () => {
   return (
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,5 +1,11 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import Generic_Image_Card from "./Generic_Image_Card";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Generic_Image_Card />
+    </Box>
+  );
 };
