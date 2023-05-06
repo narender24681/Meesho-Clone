72dc28476b426f565c3face965d532825c8f15c3
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-05-06 22:03:48 +0530

    3

--- frontend/src/Components/AllRoutes.js
+++ frontend/src/Components/AllRoutes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../Pages/Home";
 import Generic_Image_Card from "../Pages/Generic_Image_Card";
+import Women from "./Women";
 
 export const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route  path="/" element={<Home />} />
+      
     </Routes>
   );
 };
